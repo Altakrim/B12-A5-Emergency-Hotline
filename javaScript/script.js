07d04bc369fcde3f8f1b9e76971ab44e3a718144
Altakrim <emd34057gmail.com>
2025-08-28 23:26:28 -1200

    Project complate

--- javaScript/script.js
+++ javaScript/script.js
@@ -10,3 +10,36 @@ let heartCount = 0;
       heartCountEl.textContent = heartCount; 
     });
   });
+
+  let coins = 100;
+  const coinEl = document.getElementById("coinCount");
+  const callHistoryEl = document.getElementById("callHistory");
+
+  document.addEventListener("click", function (e) {
+    const btn = e.target;
+    if (btn.dataset.action === "call") {
+      const serviceName = btn.dataset.name;
+      const number = btn.dataset.number;
+
+      if (coins < 20) {
+        alert(" Not enough coins to make a call!");
+        return; 
+      }
+
+      alert(`Calling ${serviceName} (${number})`);
+      coins -= 20;
+      coinEl.textContent = coins;
+
+      const li = document.createElement("li");
+      li.textContent = `${serviceName} - ${number}`;
+      callHistoryEl.appendChild(li);
+
+      window.location.href = `tel:${number}`;
+    }
+     if (btn.dataset.action === "copy") {
+      const number = btn.dataset.number;
+      navigator.clipboard.writeText(number).then(() => {
+        alert("Copied: " + number);
+      });
+    }
+  });
